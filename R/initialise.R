ddcf5f17ea44c4440658da7649234539390e358c
andrie <apdevries@gmail.com>
2010-11-15 22:05:39 +0000

    Update documentation

--- R/initialise.R
+++ R/initialise.R
@@ -3,18 +3,18 @@
 
 
 surveyor_initialise <- function(){
-	output_to_latex <- FALSE
-	size_theme_default <- 12
+	output_to_latex <<- FALSE
+	size_theme_default <<- 12
 	set_colour_area()
 	
-	colour_default_area  = rgb(127,201,127, 255, maxColorValue=255)
-	colour_default_point = rgb(27, 158, 119, 255, maxColorValue=255)
+	colour_default_area <- rgb(127,201,127, 255, maxColorValue=255)
+	colour_default_point <- rgb(27, 158, 119, 255, maxColorValue=255)
 	
 	update_geom_defaults("bar", aes_string(fill="colour_default_area"))
 	update_geom_defaults("point", aes_string(fill="colour_default_point",
 					colour="colour_default_point"))
 	
-	ggsprint <- ggsaveprint()
+	ggsprint <<- ggsaveprint()
 }
 
 
