932894168ca0ceb997af3bcf5c5921d9e2c52fe7
andrie <apdevries@gmail.com>
2010-11-15 22:20:28 +0000

    Add demo and tests

---
+++ inst/tests/tests.R
@@ -0,0 +1,15 @@
+# This file contains test scripts, using the testthat package, developed by 
+# Hadley Wickham:
+# https://github.com/hadley/devtools/wiki/Testing
+# 
+# Author: Andrie
+###############################################################################
+
+
+
+test_that("something pointless works", {
+			
+	expect_that(1, is_a("numeric"))
+})
+
+
