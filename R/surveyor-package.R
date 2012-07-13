ec0161022003e9c54d28c6c814e27f69cc72acf8
andrie <apdevries@gmail.com>
2012-07-13 13:02:33 +0100

    Updated importFrom statements in namespace

--- R/surveyor-package.R
+++ R/surveyor-package.R
@@ -43,8 +43,10 @@
 #' @name surveyor-package
 #' @aliases surveyor surveyor-package
 #' @docType package
-#' @import  surveydata reshape2 xtable stringr lattice latticeExtra data.table ggplot2 RColorBrewer grid plyr
+#' @import  surveydata xtable lattice latticeExtra data.table ggplot2 RColorBrewer grid
 #' @importFrom Hmisc latexTranslate
+#' @importFrom plyr quickdf summarise ddply round_any
+#' @importFrom stringr str_wrap
 #' @title Tools for analysing and reporting on survey objects.
 #' @author Andrie de Vries \email{andrie.de.vries@@pentalibra.com}
 #' @keywords package
